d137fe380e49f06746af914c63380b60d1c178e9
paul-lukas <Paul-Lukas.Sternecker@gmx.de>
2023-02-09 14:45:27 +0100

    Try Push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,15 @@
 package org.example;
 
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Wie geht's");
-    }
-    int a;
-    int b;
 
+    int a = 10;
+    int b = 5;
+    int ergebniss = a - b;
+    System.out.print(ergebniss);
+    }
 }
